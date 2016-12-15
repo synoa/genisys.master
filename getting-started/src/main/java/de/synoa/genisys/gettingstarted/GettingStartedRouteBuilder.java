13943aabed4d2a6fe04eaaeea0a486d0eed2e835
Oliver Koch <o.koch@synoa.de>
2016-12-15 09:38:47 +0100

    minor changes for a working 'Hello World'

--- getting-started/src/main/java/de/synoa/genisys/gettingstarted/GettingStartedRouteBuilder.java
+++ getting-started/src/main/java/de/synoa/genisys/gettingstarted/GettingStartedRouteBuilder.java
@@ -1,17 +1,19 @@
 package de.synoa.genisys.gettingstarted;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
 
+@Component
 public class GettingStartedRouteBuilder extends RouteBuilder {
 
-	@Override
-	public void configure() throws Exception {
+    @Override
+    public void configure() throws Exception {
         // @formatter:off
 		
-		from("file:")
-			.log("Hello ${body}");
+		from("timer:gettingStarted?period=5000").routeId("Hello World Route")
+			.log("{{genisys.greeting}}");
 		
         // @formatter:on
-	}
+    }
 
 }
