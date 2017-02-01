3c3197a32282de24ee59fc0c058d3f839d4bfd21
Oliver Koch <o.koch@synoa.de>
2017-02-01 17:25:28 +0100

    Fixes #2
    Configuration for MongoDB and ActiveMQ is included in the archetype

--- getting-started-archetype/src/main/resources/archetype-resources/src/main/java/App.java
+++ getting-started-archetype/src/main/resources/archetype-resources/src/main/java/App.java
@@ -5,8 +5,10 @@ package ${package};
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource({ "classpath:META-INF/spring/mongodb.xml", "classpath:META-INF/spring/activemq.xml" })
 public class App {
 
     public static void main(String[] args) {
