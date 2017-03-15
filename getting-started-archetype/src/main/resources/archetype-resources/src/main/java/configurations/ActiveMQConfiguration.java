06d053dd475814066bc6a90556ff8c8acd3b01bd
Oliver Koch <o.koch@synoa.de>
2017-03-15 15:41:56 +0100

    Moved ActiveMQ- and MongoConfiguration to configurations package

--- getting-started-archetype/src/main/resources/archetype-resources/src/main/java/configurations/ActiveMQConfiguration.java
+++ getting-started-archetype/src/main/resources/archetype-resources/src/main/java/configurations/ActiveMQConfiguration.java
@@ -1,7 +1,7 @@
 #set( $symbol_pound = '#' )
 #set( $symbol_dollar = '$' )
 #set( $symbol_escape = '\' )
-package ${package}.configuration;
+package ${package}.configurations;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.activemq.camel.component.ActiveMQComponent;
--- getting-started-archetype/src/main/resources/archetype-resources/src/main/java/configurations/MongoConfiguration.java
+++ getting-started-archetype/src/main/resources/archetype-resources/src/main/java/configurations/MongoConfiguration.java
@@ -1,7 +1,7 @@
 #set( $symbol_pound = '#' )
 #set( $symbol_dollar = '$' )
 #set( $symbol_escape = '\' )
-package ${package}.configuration;
+package ${package}.configurations;
 
 import java.util.Collections;
 
